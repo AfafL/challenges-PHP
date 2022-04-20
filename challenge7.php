c2488347639f9aaa052e9a23043d3d39b7dda296
AfafZ <affef2082@gmail.com>
2022-04-20 12:14:42 +0200

    add the seventh challenge file

---
+++ challenge7.php
@@ -0,0 +1,14 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $caracteres["afaf labed"] = ["city" => "chatou", "weapon" => "crossbow"];
+
+        var_dump($caracteres);
+        ?>
+    </body>
+</html>
